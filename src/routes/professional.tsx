919a5aa2932acadbdfd9add2ecaed33c3cbfc4da
Paul Maresquier <paul@consilientlabs.com>
2026-01-29 17:27:39 -0800

    Refactor

--- src/routes/professional.tsx
+++ src/routes/professional.tsx
@@ -1,9 +1,13 @@
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/professional')({
-  component: RouteComponent,
+	component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/professional"!</div>
+  	return (
+		<div>
+			Hello "/professional"!
+		</div>
+	)
 }
