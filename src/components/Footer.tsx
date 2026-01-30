a2f58192e3714f164be89d60ab03929e17845aa0
Paul Maresquier <paul@consilientlabs.com>
2026-01-29 17:01:13 -0800

    Footer bottom

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
 	return (
-		<footer className="w-full py-6 px-4 bg-gray-900 border-t border-gray-800 text-center text-gray-500 text-sm">
+		<footer className="w-full py-6 px-4 bg-gray-900 border-t border-gray-800 text-center text-gray-500 text-sm bottom-0">
 			<p>
 				&copy; {new Date().getFullYear()} Paul Maresquier. All rights reserved. Made with ❤
 			</p>
