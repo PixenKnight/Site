72b9074b61caee65259e51080e9ee3ceced48659
Paul Maresquier <paul@consilientlabs.com>
2026-01-29 17:01:57 -0800

    /contact background

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -1,9 +1,13 @@
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/contact')({
-  component: RouteComponent,
+	component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/contact"!</div>
+	return (
+		<div className="min-h-[calc(100vh-72px-68px)] bg-gray-950 via-slate-800 to-slate-900">
+			Hello "/contact"!
+		</div>
+)
 }
