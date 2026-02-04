b76be28d517788ab98c93aa38c11c763f1f4aa82
Paul Maresquier <paul@consilientlabs.com>
2026-02-03 16:13:41 -0800

    Add label variants, refactored

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -1,9 +1,30 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
+
+import { motion, Transition } from 'motion/react'
 
 export const Route = createFileRoute('/contact')({
 	component: RouteComponent,
 })
+
+const mixedInputLabelVariants = {
+	neutral: { backgroundImage: 'linear-gradient(to right, #d1d5dc, #d1d5dc, #d1d5dc)' },
+	mixed: { backgroundImage: 'linear-gradient(to right, #fb2c36, #fd9a00, #fd9a00)' },
+	error: { backgroundImage: 'linear-gradient(to right, #fb2c36, #fb2c36, #fb2c36)' },
+	warning: { backgroundImage: 'linear-gradient(to right, #fd9a00, #fd9a00, #fd9a00)' },
+}
+
+const errorLabelVariants = {
+	valid: { color: '#d1d5dc' },
+	invalid: { color: '#fb2c36' },
+}
+
+const labelTransition: Transition = {
+	duration: 0.3,
+	ease: "easeInOut"
+}
+
+const labelClassName = "mb-2 text-xl font-bold"
 
 function RouteComponent() {
 	// Required for submission
@@ -24,6 +45,7 @@ function RouteComponent() {
 	const [ phoneValid, setPhoneValid ] = useState(true)
 
 	const [ preferredContact, setPreferredContact ] = useState('email')
+	const [ contactLabelVariant, setContactLabelVariant ] = useState('neutral')
 
 	useEffect(() => {
 		setNameValid(name.length > 0)
@@ -55,25 +77,17 @@ function RouteComponent() {
 		}
 	}, [nameValid, name, emailValid, email, msgValid, msg])
 
-	const validInputLabel = (validVar: boolean, touchedVar: boolean) => {
-		if (validVar || !touchedVar) {
-			return "mb-2 text-xl font-bold text-gray-300"
+	useEffect (() => {
+		if ((!emailValid && emailTouched) && (!phoneValid && phoneTouched)) {
+			setContactLabelVariant('mixed')
+		} else if (!emailValid && emailTouched) {
+			setContactLabelVariant('error')
+		} else if (!phoneValid && phoneTouched) {
+			setContactLabelVariant('warning')
 		} else {
-			return "mb-2 text-xl font-bold text-red-500"
+			setContactLabelVariant('neutral')
 		}
-	}
-
-	const mixedInputLabel = (errorVar: boolean, touchedErrorVar: boolean, warningVar: boolean, touchedWarningVar: boolean) => {
-		let className = "mb-2 text-xl font-bold "
-		if (!errorVar && touchedErrorVar) {
-			className += "text-red-500 "
-		} else if (!warningVar && touchedWarningVar) {
-			className += "text-yellow-500 "
-		} else {
-			className += "text-gray-300 "
-		}
-		return className
-	}
+	}, [emailValid, emailTouched, phoneValid, phoneTouched])
 
 	const validInputBox = (validVar: boolean, touchedVar: boolean) => {
 		if (validVar || !touchedVar) {
@@ -88,6 +102,18 @@ function RouteComponent() {
 			return "flex-1 p-3 rounded-lg bg-gray-700 text-gray-200 border border-gray-600 focus:outline-2 focus:outline-offset-2 focus:outline-cyan-500"
 		} else {
 			return "flex-1 p-3 rounded-lg bg-gray-700 text-gray-200 border border-yellow-500 focus:outline-2 focus:outline-offset-2 focus:outline-yellow-500 focus:border-transparent"
+		}
+	}
+
+	const onClickSubmit = (event: React.SubmitEvent) => {
+		if (!allowSubmit) {
+			event.preventDefault()
+			setNameTouched(true)
+			setEmailTouched(true)
+			setMsgTouched(true)
+			return false
+		} else {
+			return true
 		}
 	}
 
@@ -110,21 +136,25 @@ function RouteComponent() {
 					action="https://formsubmit.co/331aea2cbe5fd9355b7df06e1a2e5456"
 					method="POST"
 					className="flex flex-col gap-6 mb-0"
+					onSubmit={(event) => { return onClickSubmit(event) }}
 				>
 					<div className="flex flex-col" id="name-div">
-						<label
+						<motion.label
 							htmlFor="name"
-							className={`${validInputLabel(nameValid, nameTouched)}`}
+							className={labelClassName}
+							transition={labelTransition}
+							variants={errorLabelVariants}
+							initial="valid"
+							animate={(nameValid || !nameTouched) ? "valid" : "invalid"}
 						>
 							Name
-						</label>
+						</motion.label>
 						<input
 							type="text"
 							id="name"
 							name="name"
 							placeholder="Your Name"
-							required
-							title={"Please enter your name." + (nameTouched && !nameValid ? " This field is required." : "")}
+							title={"Please enter your name. This field is required."}
 							value={name}
 							onChange={(e) => setName(e.target.value)}
 							onBlur={(_) => setNameTouched(true)}
@@ -136,31 +166,34 @@ function RouteComponent() {
 							htmlFor="pronouns"
 							className="mb-2 text-xl font-bold text-gray-300"
 						>
-							Pronouns (optional)
+							Pronouns
 						</label>
 						<input
 							type="text"
 							id="pronouns"
 							name="pronouns"
-							placeholder="e.g., they/them, she/her, he/him..."
+							placeholder="e.g., they/them, she/her, he/him... (optional)"
 							className="flex-1 p-3 rounded-lg bg-gray-700 text-gray-200 border border-gray-600 focus:outline-2 focus:outline-offset-2 focus:outline-cyan-500"
 						/>
 					</div>
 					<div className="flex flex-col" id="contact-div">
-						<label
+						<motion.label
 							htmlFor="contact"
-							className={`${mixedInputLabel(emailValid, emailTouched, phoneValid, phoneTouched)}`}
+							className={`mb-2 text-xl font-bold bg-clip-text text-transparent`}
+							transition={labelTransition}
+							variants={mixedInputLabelVariants}
+							initial="neutral"
+							animate={contactLabelVariant}
 						>
 							Contact Information
-						</label>
+						</motion.label>
 						<div className="flex space-x-4 not-md:gap-5 not-md:flex-col">
 							<input
 								type="email"
 								id="email"
 								name="email"
 								placeholder="Email Address"
-								required
-								title={"Please enter your email address." + (emailTouched && !emailValid ? " This field is required and must be a valid email address." : "")}
+								title={"Please enter your email address. This field is required and must be a valid email address."}
 								value={email}
 								onChange={(e) => setEmail(e.target.value)}
 								onBlur={(_) => setEmailTouched(true)}
@@ -171,7 +204,7 @@ function RouteComponent() {
 								id="phone"
 								name="phone"
 								placeholder="Phone Number (optional)"
-								title={"Please enter your phone number." + (phoneTouched && !phoneValid ? " Due to the amount of possibilities for valid numbers, I'm only warning you that this phone MAY not be correct. If you're sure this number is correct, please proceed." : "")}
+								title={"Please enter your phone number. Due to the amount of possibilities for valid numbers, I'm only warning you that this phone MAY not be correct. If you're sure this number is correct, please proceed."}
 								value={phone}
 								onChange={(e) => setPhone(e.target.value)}
 								onBlur={(_) => setPhoneTouched(true)}
@@ -224,19 +257,22 @@ function RouteComponent() {
 						)}
 					</div>
 					<div className="flex flex-col" id="message-div">
-						<label
+						<motion.label
 							htmlFor="message"
-							className={`${validInputLabel(msgValid, msgTouched)}`}
+							className={labelClassName}
+							transition={labelTransition}
+							variants={errorLabelVariants}
+							initial="valid"
+							animate={(msgValid || !msgTouched) ? "valid" : "invalid"}
 						>
 							Message
-						</label>
+						</motion.label>
 						<textarea
 							id="message"
 							name="message"
 							rows={6}
 							placeholder="Reason for contact, questions, or additional information..."
-							required
-							title={"Please enter your message." + (msgTouched && !msgValid ? " This field is required." : "")}
+							title={"Please enter your message. This field is required."}
 							value={msg}
 							onChange={(e) => setMsg(e.target.value)}
 							onBlur={(_) => setMsgTouched(true)}
@@ -246,8 +282,7 @@ function RouteComponent() {
 					<div className="flex justify-end">
 						<button
 							type="submit"
-							disabled={!allowSubmit}
-							className="px-6 py-3 bg-cyan-600 hover:not-disabled:bg-cyan-700 text-white font-bold rounded-lg transition-colors disabled:opacity-50 disabled:cursor-not-allowed"
+							className="px-6 py-3 bg-cyan-600 hover:bg-cyan-700 text-white font-bold rounded-lg transition-colors"
 						>
 							Send Message
 						</button>
