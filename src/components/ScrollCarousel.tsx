f011a3e8a8894776dbd349baab7b3fccae051ac7
Paul Maresquier <paul@consilientlabs.com>
2026-02-05 21:49:31 -0800

    Begin comments

--- src/components/ScrollCarousel.tsx
+++ src/components/ScrollCarousel.tsx
@@ -50,7 +50,7 @@ const leftInset = `10%`
 const rightInset = `90%`
 const transparent = `#0000`
 const opaque = `#000`
-function useScrollOverflowMask(scrollXProgress: MotionValue<number>) {
+function scrollOverflowMask(scrollXProgress: MotionValue<number>) {
     const maskImage = useMotionValue(
         `linear-gradient(90deg, ${opaque}, ${opaque} ${left}, ${opaque} ${rightInset}, ${transparent})`
     )
@@ -82,7 +82,16 @@ function useScrollOverflowMask(scrollXProgress: MotionValue<number>) {
     return maskImage
 }
 
-function CarouselLi({ photo, altText, refList, viewList, index, selected }: { 
+/**
+ * Helper component for {@link ScrollCarousel}.
+ * Renders each photo and alt text, and sets up refs and inView state for each photo to be used by ScrollCarousel.
+ * 
+ * @param props.photo URL to a photo to be rendered. Relative to base URL.
+ * @param props.altText Alt text for the photo, to be shown on hover and used for accessibility.
+ * @param props.refList List of refs to be used for tracking by the parent `ScrollCarousel` component.
+ * @returns 
+ */
+function CarouselLi(props: { 
 	photo: string,
 	altText: string,
 	refList: React.RefObject<(HTMLLIElement | null)[]>,
@@ -94,12 +103,12 @@ function CarouselLi({ photo, altText, refList, viewList, index, selected }: {
 	const isInView = useInView(ref, { amount: 0.9 })
 
 	useEffect(() => {
-		refList.current[index] = ref.current
-	}, [index, refList]);
+		props.refList.current[props.index] = ref.current
+	}, [props.index, props.refList]);
 
 	useEffect(() => {
-		viewList.current[index] = isInView
-	}, [isInView, index, viewList])
+		props.viewList.current[props.index] = isInView
+	}, [isInView, props.index, props.viewList])
 
 	return (
 		<motion.li 
@@ -107,8 +116,8 @@ function CarouselLi({ photo, altText, refList, viewList, index, selected }: {
 			ref={ref}
 		>
 			<Photo
-				src={photo}
-				alt={altText}
+				src={props.photo}
+				alt={props.altText}
 				hoverAltEnabled
 				tailwindClasses={{
 					imgClasses: "rounded-lg h-100 outline-2 outline-offset-4 select-none",
@@ -117,7 +126,7 @@ function CarouselLi({ photo, altText, refList, viewList, index, selected }: {
 				props={{
 					imgProps: {
 						animate: {
-							outlineColor: selected === index ? "rgba(0, 184, 219, 1)" : "rgba(0, 184, 219, 0)"
+							outlineColor: props.selected === props.index ? "rgba(0, 184, 219, 1)" : "rgba(0, 184, 219, 0)"
 						},
 						initial: {
 							outlineColor: "transparent"
@@ -130,63 +139,106 @@ function CarouselLi({ photo, altText, refList, viewList, index, selected }: {
 	)
 }
 
-
-export default function ScrollCarousel({ photos, altTexts }: { photos: string[], altTexts: string[] }) {
+/**
+ * Simple photo carousel component.
+ * Buttons for left and right scrolling, as well as clickable indicator dots for each image and the currently selected image.
+ * Also allows manual scrolling through the scrollbar, touch controls, or mouse wheel.
+ * 
+ * See also:
+ * - {@link CarouselLi}: Inner component that renders each photo for the carousel. Uses {@link Photo}.
+ * - {@link Dots}: Inner component that renders the indicator dots.
+ * - {@link scrollOverflowMask}: Helper function that masks the edges of the carousel with a linear gradient.
+ * - {@link Photo}: Component used to render each photo in {@link CarouselLi}.
+ * 
+ * @param {string[]} props.photos Array of photo URLs to display in the carousel. Relative to base site URL.
+ * @param {string[]} props.altTexts Array of alt texts for each photo, in the same order as the photos. One-to-one correspondence with `props.photos`. 
+ * @returns 
+ */
+export default function ScrollCarousel(props: { photos: string[], altTexts: string[] }) {
 	const ulRef = useRef<HTMLUListElement>(null)
 	const { scrollXProgress } = useScroll({ container: ulRef })
-	const maskImage = useScrollOverflowMask(scrollXProgress)
+	const maskImage = scrollOverflowMask(scrollXProgress)
 
 	const [ selected, setSelected ] = useState(0)
 	const liRefs = useRef<(HTMLLIElement | null)[]>([]);
 	const liViewRefs = useRef<(boolean | null)[]>([]);
 
-	const isUserScrolling = useRef(false);
-	const scrollTimeout = useRef<number | null>(null);
+	const isUserDiscreteScrolling = useRef(false);
+	const discreteScrollTimeout = useRef<number | null>(null);
 
-	const setUserScrolling = () => {
-		isUserScrolling.current = true
-		if (scrollTimeout.current) {
-			window.clearTimeout(scrollTimeout.current)
+	// Helper function to set isUserDiscreteScrolling to true for 100 ms after a discrete scroll (i.e. through the buttons or a scroll wheel)
+	const setUserDiscreteScrolling = () => {
+		isUserDiscreteScrolling.current = true
+
+		// Clear old timeout
+		if (discreteScrollTimeout.current) {
+			window.clearTimeout(discreteScrollTimeout.current)
 		}
-		scrollTimeout.current = window.setTimeout(() => {
-			isUserScrolling.current = false
-			scrollTimeout.current = null
+
+		// Set new timeout, clears itself
+		discreteScrollTimeout.current = window.setTimeout(() => {
+			isUserDiscreteScrolling.current = false
+			discreteScrollTimeout.current = null
 		}, 100)
 	}
 
+	// useEffect to scroll to whatever element is selected if the user is scrolling with a wheel or moving the carousel through the buttons
+	// Does not trigger if the carousel is being moved with the scrollbar or through touch controls
 	useEffect(() => {
+		// Only trigger move if moving in discrete steps (i.e. through the buttons or a scroll wheel)
+		// Don't trigger if using smooth steps (i.e. through the scrollbar or touch controls)
 		const curr = liRefs.current[selected]
-		if (!curr || !isUserScrolling.current) return;
+		if (!curr || !isUserDiscreteScrolling.current) return;
+
+		// Smooth-scroll to whatever element is selected
 		curr?.scrollIntoView({ behavior: "smooth", inline: "center" })
 	}, [selected])
 
+	// useEffect to set up event listener for mouse wheels to scroll smoothly
+	// This way we can set a single scroll to be a scroll to the next/previous element, instead of it only scrolling once due to snap rules
 	useEffect (() => {
+		// Ensure carousel list has been initialized
 		const ul = ulRef.current
 		if (!ul) return;
 
+		// Event listener to specifically handle mouse wheel
 		ul.addEventListener("wheel", (e) => {
+			// Stop default scroll behavior
 			e.preventDefault()
 
-			setUserScrolling()
+			// User scrolling effect will handle smooth scroll to the next/previous element
+			setUserDiscreteScrolling()
 
+			// Auto-scroll to next/previous element, depending on direction of scroll
 			const delta = Math.sign(e.deltaY)
-			setSelected(prev => Math.max(0, Math.min(prev + delta, photos.length - 1)))
-		})
+			setSelected(prev => Math.max(0, Math.min(prev + delta, props.photos.length - 1)))
+		}, false)
 
+		// Prevent odd behavior on load where the carousel isn't at the first element
 		ul.scrollTo({ left: 0, behavior: "smooth" })
+
+		return () => {
+			// Prevent duplicate event listeners when reloading
+			ul.removeEventListener("wheel", () => {}, false)
+		}
 	}, [])
 
+	// useEffect to handle smooth motion, be it with the scrollbar or touch controls
 	useMotionValueEvent(scrollXProgress, "change", () => {
-		if (!isUserScrolling.current) {
+		// Check if user is currently not scrolling in discrete steps (i.e. through the scrollbar or touch controls)
+		if (!isUserDiscreteScrolling.current) {
+			// Select whichever element is the first one currently in view
 			const inViewIndex = liViewRefs.current.findIndex(inView => inView === true)
 			if (inViewIndex !== -1 && inViewIndex !== selected) {
 				setSelected(inViewIndex)
 			}
-		} else if (scrollTimeout.current) {
-			window.clearTimeout(scrollTimeout.current)
-			scrollTimeout.current = window.setTimeout(() => {
-				isUserScrolling.current = false
-				scrollTimeout.current = null
+		} else if (discreteScrollTimeout.current) {
+			// User is currently scrolling in discrete amounts, so prevent this event from firing until 100 ms after the scroll has ended
+			// Prevents this event from firing in the middle of a discrete scroll, leading to odd bounces
+			window.clearTimeout(discreteScrollTimeout.current)
+			discreteScrollTimeout.current = window.setTimeout(() => {
+				isUserDiscreteScrolling.current = false
+				discreteScrollTimeout.current = null
 			}, 100)
 		}
 	})
@@ -194,12 +246,12 @@ export default function ScrollCarousel({ photos, altTexts }: { photos: string[],
 	return (
 		<div className="relative w-full md:max-w-[50%] not-md:max-w-[90%] flex flex-col">
 			<motion.ul style={{ maskImage }} ref={ulRef} className="flex list-none overflow-x-auto flex-shrink-0 flex-grow-0 pb-6 mb-1 pt-6 snap-x snap-mandatory max-h-128 snap-normal">
-				{photos.map((photo, i) => {
+				{props.photos.map((photo, i) => {
 					return (
 						<CarouselLi
 							key={i}
 							photo={photo}
-							altText={altTexts[i]}
+							altText={props.altTexts[i]}
 							refList={liRefs}
 							viewList={liViewRefs}
 							index={i}
@@ -218,7 +270,7 @@ export default function ScrollCarousel({ photos, altTexts }: { photos: string[],
 						onClick={() => {
 							setSelected(prev => {
 								const newSelected = Math.max(prev - 1, 0);
-								setUserScrolling();
+								setUserDiscreteScrolling();
 								return newSelected;
 							});
 						}}
@@ -232,8 +284,8 @@ export default function ScrollCarousel({ photos, altTexts }: { photos: string[],
 						transition={{ duration: 0.2, ease: "easeInOut" }}
 						onClick={() => {
 							setSelected(prev => {
-								const newSelected = Math.min(prev + 1, photos.length - 1);
-								setUserScrolling();
+								const newSelected = Math.min(prev + 1, props.photos.length - 1);
+								setUserDiscreteScrolling();
 								return newSelected;
 							});
 						}}
@@ -241,7 +293,7 @@ export default function ScrollCarousel({ photos, altTexts }: { photos: string[],
 						<ChevronRight size={24} className="pl-[0.1rem]"/>
 					</motion.button>
 				</div>
-				<Dots count={photos.length} selected={selected} setSelected={setSelected} setUserScrolling={setUserScrolling}/>
+				<Dots count={props.photos.length} selected={selected} setSelected={setSelected} setUserScrolling={setUserDiscreteScrolling}/>
 			</div>
 		</div>
 	)
