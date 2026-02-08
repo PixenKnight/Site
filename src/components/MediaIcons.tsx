b12af85275c34d1aee4ef1287f33ca89d6cfec41
Paul Maresquier <paul@consilientlabs.com>
2026-02-07 20:10:08 -0800

    Extended MediaIcons

--- src/components/MediaIcons.tsx
+++ src/components/MediaIcons.tsx
@@ -5,7 +5,134 @@ export type IconType = "github" | "linkedin" | "steam";
 export type MediaIconsProps = {
 	size?: number;
 	exclude?: IconType | IconType[];
+	extended?: boolean;
 };
+
+export type IconProps = {
+	size?: number;
+	extended?: boolean;
+}
+
+function GitHubIcon(props: IconProps) {
+	const size = props.size ?? 20;
+
+	return (
+		<a
+			href="https://github.com/PixenKnight/Site"
+			title="GitHub"
+			target="_blank"
+			rel="noopener noreferrer"
+			className="hover:text-gray-300 text-gray-400 transition-colors cursor-pointer"
+		>
+			{props.extended ? (
+				<button
+					className="flex items-center gap-2 px-4 py-3 bg-slate-900 hover:bg-slate-800 rounded-lg border-2 border-slate-700 hover:border-slate-600 cursor-pointer"
+				>
+					<SiGithub name="githubIcon" size={size} className="cursor-pointer" />
+					<label htmlFor="githubIcon" className="cursor-pointer">
+						GitHub
+					</label>
+				</button>
+			) : (
+				<SiGithub size={size}/>
+			)}
+		</a>
+	)
+}
+
+function LinkedInIcon(props: IconProps) {
+	const size = props.size ?? 20;
+
+	return (
+
+		<a
+			href="https://www.linkedin.com/in/paul-mares"
+			title="LinkedIn"
+			target="_blank"
+			rel="noopener noreferrer"
+			className="fill-gray-400 text-gray-400 hover:fill-gray-300 hover:text-gray-300 transition-colors cursor-pointer"
+		>
+			{props.extended ? (
+				<button
+					className="flex items-center gap-2 px-4 py-3 bg-slate-900 hover:bg-slate-800 rounded-lg border-2 border-slate-700 hover:border-slate-600 cursor-pointer"
+				>
+					<svg
+						role="img"
+						aria-label="LinkedIn"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 72 72"
+						width={size}
+						height={size}
+						fill="inherit"
+					>
+						<g xmlns="http://www.w3.org/2000/svg" fillRule="evenodd">
+							<path
+								d="M8,72 L64,72 C68.418278,72 72,68.418278 72,64 L72,8 C72,3.581722 68.418278,-8.11624501e-16 64,0 L8,0 C3.581722,8.11624501e-16 -5.41083001e-16,3.581722 0,8 L0,64 C5.41083001e-16,68.418278 3.581722,72 8,72 Z"
+								fill="inherit"
+							/>
+							<path
+								d="M62,62 L51.315625,62 L51.315625,43.8021149 C51.315625,38.8127542 49.4197917,36.0245323 45.4707031,36.0245323 C41.1746094,36.0245323 38.9300781,38.9261103 38.9300781,43.8021149 L38.9300781,62 L28.6333333,62 L28.6333333,27.3333333 L38.9300781,27.3333333 L38.9300781,32.0029283 C38.9300781,32.0029283 42.0260417,26.2742151 49.3825521,26.2742151 C56.7356771,26.2742151 62,30.7644705 62,40.051212 L62,62 Z M16.349349,22.7940133 C12.8420573,22.7940133 10,19.9296567 10,16.3970067 C10,12.8643566 12.8420573,10 16.349349,10 C19.8566406,10 22.6970052,12.8643566 22.6970052,16.3970067 C22.6970052,19.9296567 19.8566406,22.7940133 16.349349,22.7940133 Z M11.0325521,62 L21.769401,62 L21.769401,27.3333333 L11.0325521,27.3333333 L11.0325521,62 Z"
+								className="fill-slate-900 hover:fill-slate-800 transition-colors"
+							/>
+						</g>
+					</svg>
+					<label htmlFor="linkedinIcon" className="cursor-pointer">
+						LinkedIn
+					</label>
+				</button>
+			) : (
+				<svg
+					role="img"
+					aria-label="LinkedIn"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 72 72"
+					width={size}
+					height={size}
+					fill="inherit"
+				>
+					<g xmlns="http://www.w3.org/2000/svg" fillRule="evenodd">
+						<path
+							d="M8,72 L64,72 C68.418278,72 72,68.418278 72,64 L72,8 C72,3.581722 68.418278,-8.11624501e-16 64,0 L8,0 C3.581722,8.11624501e-16 -5.41083001e-16,3.581722 0,8 L0,64 C5.41083001e-16,68.418278 3.581722,72 8,72 Z"
+							fill="inherit"
+						/>
+						<path
+							d="M62,62 L51.315625,62 L51.315625,43.8021149 C51.315625,38.8127542 49.4197917,36.0245323 45.4707031,36.0245323 C41.1746094,36.0245323 38.9300781,38.9261103 38.9300781,43.8021149 L38.9300781,62 L28.6333333,62 L28.6333333,27.3333333 L38.9300781,27.3333333 L38.9300781,32.0029283 C38.9300781,32.0029283 42.0260417,26.2742151 49.3825521,26.2742151 C56.7356771,26.2742151 62,30.7644705 62,40.051212 L62,62 Z M16.349349,22.7940133 C12.8420573,22.7940133 10,19.9296567 10,16.3970067 C10,12.8643566 12.8420573,10 16.349349,10 C19.8566406,10 22.6970052,12.8643566 22.6970052,16.3970067 C22.6970052,19.9296567 19.8566406,22.7940133 16.349349,22.7940133 Z M11.0325521,62 L21.769401,62 L21.769401,27.3333333 L11.0325521,27.3333333 L11.0325521,62 Z"
+							fill="#101828"
+						/>
+					</g>
+				</svg>
+			)}
+		</a>
+	)
+}
+
+function SteamIcon(props: IconProps) {
+	const size = props.size ?? 20;
+
+	return (
+
+		<a
+			href="https://steamcommunity.com/id/PixenKnight/"
+			title="Steam"
+			target="_blank"
+			rel="noopener noreferrer"
+			className="hover:text-gray-300 text-gray-400 transition-colors"
+		>
+			{props.extended ? (
+				<button
+					className="flex items-center gap-2 px-4 py-3 bg-slate-900 hover:bg-slate-800 rounded-lg border-2 border-slate-700 hover:border-slate-600 cursor-pointer"
+				>
+					<SiSteam name="steamIcon" size={size} className="cursor-pointer" />
+					<label htmlFor="steamIcon" className="cursor-pointer">
+						Steam
+					</label>
+				</button>
+			) : (
+				<SiSteam size={size}/>
+			)}
+		</a>
+	)
+}
 
 export default function MediaIcons(props: MediaIconsProps) {
 	const iconSize = props.size ?? 20;
@@ -26,55 +153,13 @@ export default function MediaIcons(props: MediaIconsProps) {
 	return (
 		<div className="flex gap-4 text-gray-500 fill-gray-500 ">
 			{iconsToRender.github && (
-				<a
-					href="https://github.com/PixenKnight/Site"
-					title="GitHub"
-					target="_blank"
-					rel="noopener noreferrer"
-					className="hover:text-gray-300 transition-colors"
-				>
-					<SiGithub size={iconSize} />
-				</a>
+				<GitHubIcon size={iconSize} extended={props.extended}/>
 			)}
 			{iconsToRender.linkedin && (
-				<a
-					href="https://www.linkedin.com/in/paul-mares"
-					title="LinkedIn"
-					target="_blank"
-					rel="noopener noreferrer"
-					className="hover:fill-gray-300 transition-colors"
-				>
-					<svg
-						role="img"
-						aria-label="LinkedIn"
-						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 72 72"
-						width={iconSize}
-						height={iconSize}
-					>
-						<g xmlns="http://www.w3.org/2000/svg" fillRule="evenodd">
-							<path
-								d="M8,72 L64,72 C68.418278,72 72,68.418278 72,64 L72,8 C72,3.581722 68.418278,-8.11624501e-16 64,0 L8,0 C3.581722,8.11624501e-16 -5.41083001e-16,3.581722 0,8 L0,64 C5.41083001e-16,68.418278 3.581722,72 8,72 Z"
-								fill="inherit"
-							/>
-							<path
-								d="M62,62 L51.315625,62 L51.315625,43.8021149 C51.315625,38.8127542 49.4197917,36.0245323 45.4707031,36.0245323 C41.1746094,36.0245323 38.9300781,38.9261103 38.9300781,43.8021149 L38.9300781,62 L28.6333333,62 L28.6333333,27.3333333 L38.9300781,27.3333333 L38.9300781,32.0029283 C38.9300781,32.0029283 42.0260417,26.2742151 49.3825521,26.2742151 C56.7356771,26.2742151 62,30.7644705 62,40.051212 L62,62 Z M16.349349,22.7940133 C12.8420573,22.7940133 10,19.9296567 10,16.3970067 C10,12.8643566 12.8420573,10 16.349349,10 C19.8566406,10 22.6970052,12.8643566 22.6970052,16.3970067 C22.6970052,19.9296567 19.8566406,22.7940133 16.349349,22.7940133 Z M11.0325521,62 L21.769401,62 L21.769401,27.3333333 L11.0325521,27.3333333 L11.0325521,62 Z"
-								fill="#101828"
-							/>
-						</g>
-					</svg>
-				</a>
+				<LinkedInIcon size={iconSize} extended={props.extended}/>
 			)}
 			{iconsToRender.steam && (
-				<a
-					href="https://steamcommunity.com/id/PixenKnight/"
-					title="Steam"
-					target="_blank"
-					rel="noopener noreferrer"
-					className="hover:text-gray-300 transition-colors"
-				>
-					<SiSteam size={iconSize} />
-				</a>
+				<SteamIcon size={iconSize} extended={props.extended}/>
 			)}
 		</div>
 	);
