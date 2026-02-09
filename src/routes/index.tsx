da949352e4841e8ab9580b0358707446096916de
Paul Maresquier <paul@consilientlabs.com>
2026-02-09 08:08:54 -0800

    Switch to pure motion anim, no tw custom anim

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,10 +5,30 @@ import MediaIcons from "@/components/MediaIcons";
 export const Route = createFileRoute("/")({ component: App });
 
 function App() {
+	const mainBGOpacity = 0.7;
+	const lowBGOpacity = 0.4;
+
 	return (
 		<div className="min-h-[calc(100vh-72px-68px)] bg-gray-950 via-slate-800 to-slate-900">
 			<section className="relative py-20 px-6 text-center overflow-hidden h-[calc(100vh-72px)] flex flex-col justify-center">
-				<div className="absolute inset-0 bg-gradient-to-r from-cyan-500 via-blue-500 to-purple-500 animate-slow-pulse"></div>
+				<motion.div
+					className="absolute inset-0"
+					animate={{
+						backgroundImage: [
+							`linear-gradient(to right, rgba(0, 184, 219, ${lowBGOpacity}), rgba(43, 127, 255, ${mainBGOpacity}), rgba(173, 70, 255, ${mainBGOpacity}))`,
+							`linear-gradient(to right, rgba(0, 184, 219, ${mainBGOpacity}), rgba(43, 127, 255, ${lowBGOpacity}), rgba(173, 70, 255, ${mainBGOpacity}))`,
+							`linear-gradient(to right, rgba(0, 184, 219, ${mainBGOpacity}), rgba(43, 127, 255, ${mainBGOpacity}), rgba(173, 70, 255, ${lowBGOpacity}))`,
+							`linear-gradient(to right, rgba(0, 184, 219, ${mainBGOpacity}), rgba(43, 127, 255, ${lowBGOpacity}), rgba(173, 70, 255, ${mainBGOpacity}))`,
+							`linear-gradient(to right, rgba(0, 184, 219, ${lowBGOpacity}), rgba(43, 127, 255, ${mainBGOpacity}), rgba(173, 70, 255, ${mainBGOpacity}))`,
+						]
+					}}
+					initial={{ backgroundImage: `linear-gradient(to right, rgba(0, 184, 219, ${lowBGOpacity}), rgba(43, 127, 255, ${mainBGOpacity}), rgba(173, 70, 255, ${mainBGOpacity}))` }}
+					transition={{
+						duration: 15,
+						repeat: Infinity,
+						times: [0, 0.25, 0.5, 0.75, 1],
+					}}
+				/>
 				<div className="relative flex flex-col items-center max-w-5xl mx-auto">
 					<motion.div
 						className="flex items-center justify-center gap-6 mb-6 w-fit"
@@ -37,42 +57,11 @@ function App() {
 						I'm a full-stack developer who loves building robust, scalable
 						infrastructure
 					</p>
-					<p className="text-lg text-gray-400 max-w-3xl mx-auto mb-8">
+					<p className="text-lg text-gray-300 max-w-3xl mx-auto mb-8">
 						Welcome to my personal website!
 					</p>
 					<MediaIcons size={30} extended />
 				</div>
-				{/* <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 mb-4">
-					<motion.span
-						className="text-gray-200 flex flex-col items-center hover:cursor-pointer"
-						animate={{
-							y: 0,
-							opacity: 1,
-						}}
-						initial={{
-							y: -10,
-							opacity: 0,
-						}}
-						transition={{
-							duration: 0.5,
-							ease: "easeOut",
-							delay: 0.2,
-						}}
-						whileHover={{
-							y: -5,
-							transition: {
-								duration: 0.3,
-								ease: "easeInOut",
-							},
-						}}
-						onClick={() => {
-							window.scrollTo({ top: window.innerHeight, behavior: "smooth" });
-						}}
-					>
-						<p className="mb-[-0.5rem]">Scroll Down</p>
-						<p>&#x2304;</p>
-					</motion.span>
-				</div> */}
 			</section>
 		</div>
 	);
